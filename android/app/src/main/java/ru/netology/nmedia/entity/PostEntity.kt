787119d0757ad981dfdcf950bea42292b695336f
Pavel A. <pavelanorov@yandex.ru>
2024-03-27 15:31:53 +0300

    ver 4

---
+++ android/app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
@@ -0,0 +1,55 @@
+package ru.netology.nmedia.entity
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.netology.nmedia.dto.Post
+
+@Entity
+data class PostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val authorId: Long,
+    val author: String,
+    val authorAvatar: String,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    @Embedded
+    var attachment: AttachmentEmbeddable?,
+    val ownedByMe: Boolean = false,
+) {
+    fun toDto() = Post(
+        id = id,
+        authorId = authorId,
+        author = author,
+        authorAvatar = authorAvatar,
+        content = content,
+        published = published,
+        likedByMe = likedByMe,
+        likes = likes,
+        attachment = attachment?.toDto(),
+        ownedByMe = ownedByMe
+    )
+
+    companion object {
+        fun fromDto(dto: Post) =
+            PostEntity(
+                id = dto.id,
+                authorId = dto.authorId,
+                author = dto.author,
+                authorAvatar = dto.authorAvatar,
+                content = dto.content,
+                published = dto.published,
+                likedByMe = dto.likedByMe,
+                likes = dto.likes,
+                attachment = AttachmentEmbeddable.fromDto(dto.attachment),
+                ownedByMe = dto.ownedByMe
+            )
+
+    }
+}
+
+fun List<PostEntity>.toDto(): List<Post> = map(PostEntity::toDto)
+fun List<Post>.toEntity(): List<PostEntity> = map(PostEntity::fromDto)
